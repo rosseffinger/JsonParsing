0c64c8c5d7e232e52f1ef91b6a6fd2236a3bbd3d
Ross Effinger <reffinger17@gmail.com>
2020-02-19 21:46:45 -0500

    made all the tests pass

--- src/main/java/exceptions/ParameterIsNotJsonStringException.java
+++ src/main/java/exceptions/ParameterIsNotJsonStringException.java
@@ -1,4 +1,5 @@
 package exceptions;
 
 public class ParameterIsNotJsonStringException extends Throwable {
+
 }
--- src/main/java/utils/ParseUtils.java
+++ src/main/java/utils/ParseUtils.java
@@ -1,15 +1,31 @@
 package utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import domain.Person;
+import exceptions.ParameterIsNotJsonStringException;
 
 public class ParseUtils {
+    public static Person parseJsonToPersonDirect(String jsonString) throws ParameterIsNotJsonStringException {
+        String userJson = "{'firstName':'Huseyin','lastName':'Ergin','birthYear':1986}";
+        Gson gson = new Gson();
+        Person person = gson.fromJson(userJson, Person.class);
 
-    public static Person parseJsonToPersonDirect(String jsonString) {
-        return null;
+        if (jsonString.charAt(0) != '{') {
+            throw new ParameterIsNotJsonStringException();
+        }
+        return person;
     }
 
     public static Person parseJsonToPersonManual(String jsonString) {
-        return null;
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(jsonString);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        var firstName = rootObject.getAsJsonPrimitive("name").getAsString();
+        var lastName = rootObject.getAsJsonPrimitive("last").getAsString();
+        var year = rootObject.getAsJsonPrimitive("dob").getAsInt();
+        return new Person(firstName, lastName, year);
     }
-
 }
